b0ddb2f198cfc2fe8605844354df37ce1d8d6c36
collincouch <collindcoucoh@gmail.com>
2019-01-21 19:18:35 -0700

    Initial commit

---
+++ screens/TestScreen.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native'
+import { WebBrowser } from 'expo'
+import { TestComponent } from './../components/AppComponents'
+
+export default class TestScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  }
+
+  render() {
+    return (
+      <View style={{ paddingTop: 20 }}>
+        <Text>Hello</Text>
+        <TestComponent />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
